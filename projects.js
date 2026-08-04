394f23650ba6f0555db5a9294b520d4b8a11717f
nayrucache <68495805+nayrucache@users.noreply.github.com>
2026-08-04 18:17:29 -0400

    Add files via upload

--- projects.js
+++ projects.js
@@ -28,4 +28,25 @@ const PROJECTS = [
   //   description: "Built with the Web Audio API. Lets you define rules that generate melodies and drum patterns.",
   //   url: "https://example.com",
   // },
+  {
+  id: "p_001",
+  title: "B&O Surround Sound for MythsEnt premiere",
+  year: 2026,
+  kind: "On-hands system routing",
+  summary: "Installed B&O Beosystem 1 surround sound system for the premiere event of the MythsEnt archival library/school.",
+  description: "Used for speeches, musical performances, visual presentations, and anything requiring audio. This involved routing each tower speaker through wall ports into a main panel housing all the ports, which fed into the console connected to the B&O system. Spent time mitigating electrical interference and streamlining each wired connection for the quickest possible response times. Connected a channel switcher between the console and sound system, which accommodated Bluetooth, wired USB-C, and USB-A connections.",
+  url: "https://mythsent.com/",
+  image: "assets/projects/IMG_5713.jpg"
+  },
+
+  {
+  id: "p_002",
+  title: "Tracking & Engineering W/DontKallMeLuxxy",
+  year: 2026,
+  kind: "Tracking/Vocal Engineering",
+  summary: "Tracked and recorded vocals for DontkallMeLuxxys “Stadiums”",
+  description: "Spent a weekend with Luxxy living in a NJ recording studio grinding tracks, it was a really fun and rewarding experience that I’m glad I could be a part of. Each day we recorded around 5-7 tracks and by the end of the trip we had 15 tracks ready to send off for mastering then release. Still engineer and record together to this day.",
+  url: "https://youtu.be/TYG2YxGns0c?si=HDm-9VLG1D_yB24C",
+  image: "assets/projects/Luxxy.jpg"
+  }
 ];
